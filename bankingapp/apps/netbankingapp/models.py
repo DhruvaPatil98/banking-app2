63476d6ecfefbc12918d16b3d533789a08fa3c07
Gautam Anil <gautamanil20july@gmail.com>
2020-02-26 11:30:52 +0530

    Added User and Account Model

--- bankingapp/apps/netbankingapp/models.py
+++ bankingapp/apps/netbankingapp/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator,
+)
+import uuid
 
 # Create your models here.
+class Users(models.Model):
+
+    id = models.UUIDField(
+        primary_key=True, unique=True, editable=False,
+        default=uuid.uuid4
+        )
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    username = models.CharField(
+        max_length=30, unique=True, null=True, blank=True,
+        help_text=(
+            'Required. 30 characters or fewer. Letters, digits and '
+            'Use special characters in case the given username is repeated'
+        ))
+
+    dob = models.DateField(blank=False, null=False)
+    address = models.CharField(max_length=100)
+
+    password = models.CharField(
+        max_length=30, unique=False, null=True, blank=True,
+        help_text=(
+              'Required. 30 characters or fewer. Letters, digits and '
+              'special characters'
+          ))
+
+    def __str__(self):
+        return str(self.username)
+
+class Account(models.Model):
+    account_id = models.UUIDField(
+                                  primary_key=True,
+                                  editable=False,
+                                  unique=True,
+                                  default=uuid.uuid4
+                                )
+    user = models.ForeignKey(
+            Users, on_delete=models.CASCADE, related_name='accounts'
+        )
+
+    #using MinValueValidator and MaxValueValidator to validate account number to have 8 digits
+
+    account_no = models.IntegerField(
+        unique=True,
+        validators=[
+            MinValueValidator(10000000),
+            MaxValueValidator(99999999)
+        ],
+        null=True, blank=True
+    )
+
+    #validating password to have special characters and upper and lower case letters and digits
+    pin = models.IntegerField(
+         unique=False, null=True, blank=True,
+         help_text=(
+              'Required. 6 digits or 4 digit number'
+          ))
+
+    balance = models.DecimalField(decimal_places=2, max_digits=15)
+
+    def __str__(self):
+        return str(self.account_no) + " balance: " + str(self.balance)
